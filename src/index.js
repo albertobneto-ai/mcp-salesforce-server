e5dc79da8126bd86482a07adac5c665ce8196fa2
albertobneto-ai <albertobneto.ai@gmail.com>
2026-08-17 15:36:07 -0300

    feat(triagem): registrar rotas triagem no index

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ import emailValidation from "./email-validation.js";
 import qaTesterRouter from "./qa-tester.js";
 import { registerCofreRoutes } from "./cofre-db.js";
 import { buddieRouter } from "./buddie.js";
+import { registerTriagemRoutes } from "./triagem-db.js";
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
@@ -2227,6 +2228,7 @@ registerRevenueCatalogRoutes(app);
 registerRcWorkerRoutes(app);
 registerWorkerEngineRoutes(app);
 registerCofreRoutes(app);
+registerTriagemRoutes(app);
   
   // TechEnglish
   app.use('/english', englishRouter);
